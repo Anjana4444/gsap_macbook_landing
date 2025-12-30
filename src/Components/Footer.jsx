081ed246246c35a694a1d50088955a1f7000c06d
Anjana Aluwihare <aaluwihare4@gmail.com>
2025-12-30 14:23:12 +0530

    implemented footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { footerLinks } from "../Constants";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop: Find an Apple Store or other retailer near you. Or
+          call 015405222
+        </p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+
+      <hr />
+
+      <div className="links">
+        <p>Copyright 2024 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
